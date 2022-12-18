771d484a0d1b10d6dfe3d4b45fd2c988104503ae
Pratik Wadekar <pratik.wadekar2810@gmail.com>
2022-12-18 12:55:06 +0530

    code changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -267,7 +267,7 @@ router.get('/github/:username', async (req, res) => {
     );
     const headers = {
       'user-agent': 'node.js',
-      Authorization: `token ${config.get('githubToken')}`
+      Authorization: `${config.get('githubToken')}`
     };
 
     const gitHubResponse = await axios.get(uri, { headers });
